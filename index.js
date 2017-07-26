ea03ee687b2845f21240730a92f7e06d2cd96a92
izcet <isaacjrhett@gmail.com>
2017-07-25 21:45:54 -0700

    getting the hang of this here script of java

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 const rapid = require("rapid-io");
-const api = require("api.key.json");
+const api = require("./api.key.json");
 const rapidClient = rapid.createClient(api.key);
-console.log(api.key);
+//console.log(api.key);
+
+function makePerson(fname, lname) {
+	var person = {
+		"firstName" : fname,
+		"lastName" : lname,
+		"toString" : function(){
+			return (this.firstName + " " + this.lastName);
+		}
+	};
+	return person;
+}
+
+function makeTransaction(person1, person2, amount, tags) {
+	var transaction = {
+		"from" : person1,
+		"to" : person2,
+		"value" : amount,
+		"tags" : tags,
+		toString : function() {
+			return (this.from.toString() + " sent " + this.to.toString() + " $" + this.value);
+		}
+	};
+	return transaction;
+}
+
+var foo = makePerson("billy", "bob");
+
+var bar = makePerson("bobby", "bill");
+
+var trans = makeTransaction(foo, bar, 100, []);
+
+console.log(foo.toString());
+console.log(bar.toString());
+console.log(trans.toString());
+		
