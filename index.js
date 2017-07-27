41226033b03ade4effd88ccc42c8cad540a0846c
Chris Renfrow <mail@chrisrenfrow.me>
2017-07-26 22:25:42 -0700

    Some basic functions

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/25 22:05:00 by irhett            #+#    #+#             */
-/*   Updated: 2017/07/25 22:17:12 by crenfrow         ###   ########.fr       */
+/*   Updated: 2017/07/26 21:24:51 by crenfrow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,32 @@ const rapid = require('rapid-io')
 
 const api = require('./key.json')
 const rapidClient = rapid.createClient(api.key)
-const userList = rapidClient.collection('user-list')
+const users = rapidClient.collection('user-list')
+const transactions = rapidClient.collection('transactions-list')
 
 function createUser (firstName, lastName) {
 // Validate input here, also query to see if we have a user by this name/id already
 
 	let id = new Date().getTime() // Lazy ID system
 	rapidClient
-		.collection(userList.id)
+		.collection(users.id)
 		.newDocument()
 		.mutate({
 			firstName: firstName,
 			lastName: lastName,
+			userName: firstName[0] + lastName,
+			balance: 0,
 			id: id
 		})
 	.then(
-		() => console.log('success'),
+		() => console.log(
+			'Created user:' +
+			'\nFirst: ' + firstName +
+			'\nLast: ' + lastName +
+			'\nUsername: ' + firstName[0] + lastName +
+			'\nBalance: ' + 0,
+			'\nid: ' + id
+		),
 		err => {
 			if (err) {
 				switch (err.type) {
@@ -42,67 +52,86 @@ function createUser (firstName, lastName) {
 
 function createTransaction (to, from, amount, tags) {
     // Insert input validation and query logic here
-	let transId = new Date().getTime() // Lazy ID system for testing
-	return ({
-		to: to,
-		from: from,
-		amount: amount,
-		id: transId
+	let id = new Date().getTime() // Lazy ID system
+	rapidClient
+		.collection(transactions.id)
+		.newDocument()
+		.mutate({
+			to: to,
+			from: from,
+			amount: amount,
+			tags: tags,
+			id: id
+		})
+	.then(
+		() => console.log(
+			'Created transaction:' +
+			'\nTo: ' + to +
+			'\nFrom: ' + from +
+			'\nAmount: ' + amount +
+			'\nID: ' + id +
+			'\nTags: ' + tags
+		),
+		err => {
+			if (err) {
+				switch (err.type) {
+				case 'timeout': break
+				case 'permission-denied': break
+				}
+			}
+		})
+}
+
+function deleteUserByUserName(userName) {
+	rapidClient
+	.collection(users.id)
+	.filter({ userName: userName })
+	.fetch(userList => {
+		for (let u in userList) {
+			rapidClient
+			.collection(users.id)
+			.document(userList[u].id)
+			.delete({ timeout: 10000 })
+			.then(
+				() => console.log('Deleted: ' + JSON.stringify(userList[u])),
+				err => console.log(err.type)
+			)
+		}
+		// console.log(userList)
 	})
 }
 
-// createUser('chris', 'renfrow')
-// createUser('isaac', 'rhett')
-
-rapidClient
-    .collection(userList.id)
-    .fetch(users => {
-        // TODO: process to-dos
-	console.log(users)
-})
-
-/*
-const rapid = require("rapid-io");
-const api = require("./api.key.json");
-const rapidClient = rapid.createClient(api.key);
-
-function makePerson(fname, lname) {
-	var person = {
-		"firstName" : fname,
-		"lastName" : lname,
-		"toString" : function(){
-			return (this.firstName + " " + this.lastName);
-		}
-	};
-	return person;
+function logAllItemsInCollection(collectionId) {
+	rapidClient
+	.collection(collectionId)
+	.fetch(items => {
+		console.log(items)
+	})
 }
 
-function makeTransaction(person1, person2, amount, tags) {
-	var transaction = {
-		"from" : person1,
-		"to" : person2,
-		"value" : amount,
-		"tags" : tags,
-		toString : function() {
-			return (this.from.toString() + " sent " + this.to.toString() + " $" + this.value);
+function deleteAllInCollection(collectionId) {
+	rapidClient
+	.collection(collectionId)
+	.fetch(items => {
+		for (let i in items) {
+			rapidClient
+			.collection(collectionId)
+			.document(items[i].id)
+			.delete({ timeout: 10000 })
+			.then(
+				() => console.log('Deleted: ' + items[i].id),
+				err => console.log(err.type)
+			)
 		}
-	};
-	return transaction;
+		// console.log(items)
+	})
 }
 
-var user = makePerson("current", "user");
-
-var foo = makePerson("billy", "bob");
-var bar = makePerson("bobby", "bill");
-
-const transactions = rapidClient.collection('transactions');
-
-
-
-
-var trans = makeTransaction(foo, bar, 100, []);
-
-console.log(foo.toString());
-console.log(bar.toString());
-console.log(trans.toString());
-*/
+createUser('chris', 'renfrow')
+createUser('isaac', 'rhett')
+createTransaction('crenfrow', 'irhett', '1337', ['whut'])
+// deleteUserByUserName('crenfrow')
+// deleteAllInCollection(transactions.id)
+// deleteAllInCollection(users.id)
+// logAllItemsInCollection(transactions.id)
+// logAllItemsInCollection(users.id)
